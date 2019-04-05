f309f19ea1da805f2b661e8ff6f84c524bed8ec4
Ryan HvA <ryan.zhang@hva.nl>
2019-04-05 15:53:06 +0200

    encoding Step 1 working

--- src/pa3/FXMLDocumentController.java
+++ src/pa3/FXMLDocumentController.java
@@ -20,7 +20,7 @@ import javafx.scene.control.TextArea;
  * @author Ryan J Z
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Button buttonStepOne;
     @FXML
@@ -31,29 +31,68 @@ public class FXMLDocumentController implements Initializable {
     private TextArea outputStep1;
     @FXML
     private TextArea outputStep2;
-    
+
     int p = 0;
     int q = 0;
-    int e = 0;
-    
+    int phi = 0;
+    int n = 0;
+
+    static void SieveOfEratosthenes(int n, boolean isPrime[]) {
+        // Makes an boolean array with weather n is a prime or not
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int p = 2; p * p <= n; p++) {
+            // If isPrime[p] is not changed, then it is a prime 
+            if (isPrime[p] == true) {
+                // Update all multiples of p 
+                for (int i = p * 2; i <= n; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+    }
+
     @FXML
     private void stepOneButtonClicked(ActionEvent event) {
+        int flag = 0;
+        
         System.out.println("Button 1 Clicked");
-        outputStep1.setText("p is " + p + "\nq is " + q);
+        
+        n = Integer.parseInt(inputStep1.getText());
+        // Generating primes using Sieve 
+        boolean[] isPrime = new boolean[n + 1];
+        SieveOfEratosthenes(n, isPrime);
+
+        // Traversing all numbers to find first pair 
+        for (int i = 2; i < n; i++) {
+            int x = n / i;
+
+            if (isPrime[i] && isPrime[x] && x != i && x * i == n) {
+                outputStep1.setText("p is " +i + "\nq is " + x);
+                flag = 1;
+                return;
+            }
+        }
+
+        if (flag == 0) {
+            outputStep1.setText("No such pair found");
+        }
+        
     }
-    
+
     @FXML
     private void stepTwoButtonClicked(ActionEvent event) {
         System.out.println("Button 2 Clicked");
-        int e = (p-1) * (q-1);
-        System.out.println(inputStep2.getText());
-        
-        outputStep2.setText("e is "+ e);
+        int phi = (p - 1) * (q - 1);
+        outputStep2.setText("");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
